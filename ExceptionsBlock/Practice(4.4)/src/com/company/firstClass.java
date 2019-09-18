f066168438fcdd4ce8e9ab0ceec0317accd80af5
Name <51792454+maksikooo@users.noreply.github.com>
2019-09-18 23:26:12 +0300

    Exceptions Practice added

---
+++ ExceptionsBlock/Practice(4.4)/src/com/company/firstClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class firstClass {
+    public void main() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+}
---
+++ ExceptionsBlock/Practice(4.4)/src/com/company/secondClass.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class secondClass {
+    public void main(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
